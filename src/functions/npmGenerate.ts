e537043e14fcc28719763383486f004a70fad78f
Chris Tate <chris@austincodeshop.com>
2019-07-12 01:00:36 -0500

    fixes generate script

--- src/functions/npmGenerate.ts
+++ src/functions/npmGenerate.ts
@@ -1,9 +1,12 @@
 import { spawn } from 'child_process';
+import * as path from 'path';
 
 export const npmGenerate = (cwd: string) => {
   return new Promise(resolve => {
     // create new daemon process
-    const child = spawn('npm', ['run', 'generate:all'], {
+    const pkg = require(path.join(cwd, 'package.json'));
+    const generateScript = pkg.scripts && pkg.scripts['generate:all'] ? 'generate:all' : 'generate';
+    const child = spawn('npm', ['run', generateScript], {
       cwd,
       stdio: ['ignore', process.stdout, process.stdout]
     });
